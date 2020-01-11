4fec93a849856c58010b4b7e74069a29bda719bb
Mike Koenig <koenigm@seattleu.edu>
2020-01-10 16:58:45 -0800

    Revert "Changed Title to Items for ItemsViewModel"
    This reverts commit 392f112ff8f4ee83e5817e570f5e8caafa654c65.

--- Mine/Mine/ViewModels/ItemsViewModel.cs
+++ Mine/Mine/ViewModels/ItemsViewModel.cs
@@ -17,7 +17,7 @@ namespace Mine.ViewModels
 
         public ItemsViewModel()
         {
-            Title = "Items";
+            Title = "Browse";
             Items = new ObservableCollection<Item>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
