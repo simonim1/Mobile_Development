fa46ab00d88824dbd4af34e55f58bf80ca6d3a4e
Michelle Simoni <michelle.simoni779@gmail.com>
2020-02-13 16:12:51 -0800

    chreated a database service file and  initialized the database

---
+++ Mine/Mine/Services/DatabaseService.cs
@@ -0,0 +1,41 @@
+﻿using SQLite;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Mine.Models;
+using System.Collections.Generic;
+
+namespace Mine.Services
+{
+    class DatabaseService
+    {
+
+        static readonly Lazy<SQLiteAsyncConnection> lazyInitializer = new Lazy<SQLiteAsyncConnection>(() =>
+        {
+            return new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        });
+
+        static SQLiteAsyncConnection Database => lazyInitializer.Value;
+        static bool initialized = false;
+
+        public DatabaseService()
+        {
+            InitializeAsync().SafeFireAndForget(false);
+        }
+
+
+        public async Task InitializeAsync()
+        {
+            if (!initialized)
+            {
+                if (!Database.TableMappings.Any(m => m.MappedType.Name == typeof(ItemModel).Name))
+                {
+                    await Database.CreateTablesAsync(CreateFlags.None, typeof(ItemModel)).ConfigureAwait(false);
+                    initialized = true;
+                }
+            }
+        }
+
+      
+    }
+}
